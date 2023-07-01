af73d1e20ee772db566663516dea6806d35e05e3
Кто-то <ivan.astashin.04@mail.ru>
2023-07-01 19:42:02 +0300

    new PersonalAccount ITactiv&Sapport

--- HelpAct/pages/uUserSapportPersonalAccount.xaml.cs
+++ HelpAct/pages/uUserSapportPersonalAccount.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,6 +56,20 @@ namespace HelpAct
         private void mExit_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Autorization());
+        }
+        private void BtnLoadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                aImage.Source = new BitmapImage(fileUri);
+            }
+        }
+
+        private void uEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
